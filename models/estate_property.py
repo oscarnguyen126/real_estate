1729f89de9e1001939429953c167c49bdc355a7f
trau <oscarnguyen126@gmail.com>
2022-12-23 18:30:59 +0700

    Add button for estate, offer

--- models/estate_property.py
+++ models/estate_property.py
@@ -1,5 +1,6 @@
-from odoo import fields, models, _
+from odoo import fields, models, _, api
 from dateutil.relativedelta import relativedelta
+from odoo.exceptions import ValidationError, UserError
 
 
 class EstateProperty(models.Model):
@@ -27,3 +28,34 @@ class EstateProperty(models.Model):
     buyer = fields.Many2one('res.partner')
     tag_ids = fields.Many2many('estate.property.tag','estate_property_tags_rel','estate_property_id','estate_property_tag_id')
     offer_ids = fields.One2many('estate.property.offer', 'property_id')
+    total_area = fields.Integer(string=_('Total area'), compute='compute_area', store=True)
+    best_price = fields.Monetary(string=_('Best price'), currency_field='currency_id', compute='compute_best_price', store=True)
+
+    @api.depends('living_area', 'garden_area')
+    def compute_area(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
+
+    @api.depends('offer_ids')
+    def compute_best_price(self):
+        for record in self:
+            if record.offer_ids:
+                record.best_price = max(record.offer_ids.mapped('price'))
+
+    @api.onchange('garden')
+    def onchange_area_and_orientation(self):
+        for record in self:
+            if record.garden is True:
+                record.garden_area = 10
+                record.garden_orientation = 'north'
+
+    def sold_button(self):
+        for record in self:
+            if record.state == 'canceled':
+                raise UserError('Canceled property cannot be sold')
+            record.state = 'sold'
+    def cancel_button(self):
+        for record in self:
+            if record.state == 'sold':
+                raise UserError('Sold property cannot be canceled')
+            record.state = 'canceled'
--- models/offers.py
+++ models/offers.py
@@ -1,4 +1,5 @@
-from odoo import models, fields, _
+from odoo import models, fields, _, api
+from dateutil.relativedelta import relativedelta
 
 
 class Offers(models.Model):
@@ -9,3 +10,21 @@ class Offers(models.Model):
     partner_id = fields.Many2one('res.partner', required=True)
     property_id = fields.Many2one('estate.property')
     currency_id = fields.Many2one('res.currency', string="Currency", default=lambda self: self.env.ref('base.VND').id)
+    validity = fields.Integer(string=_('Validity (days)'), default = 7)
+    date_deadline = fields.Date(string=_('Date Deadline'), compute='compute_deadline', store=True)
+
+    @api.depends('validity')
+    def compute_deadline(self):
+        for record in self:
+            if record.create_date:
+                record.date_deadline = record.create_date + relativedelta(days=record.validity)
+
+    def confirm_button(self):
+        for record in self:
+            record.status = 'accepted'
+            record.property_id.selling_price = record.price
+            record.property_id.buyer = record.partner_id
+
+    def reject_button(self):
+        for record in self:
+            record.status = 'refused'
